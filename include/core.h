a6369325ba890fb0438281558329ba925c74fa2d
Robbe De Greef <robbedegreef@gmail.com>
2020-11-23 17:03:03 +0100

    Added the math and list includes for our slicing needs

--- include/core.h
+++ include/core.h
@@ -2,12 +2,14 @@
 
 // C includes
 #include <stdio.h>
+#include <math.h>
 
 // C++ includes
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
+#include <list>
 
 // Custom includes
 #include <debug.h>
